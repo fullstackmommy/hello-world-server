9c3969a3d6ecdc1199a322f2e91dafce481ad0ab
fullstackmommy <nicole.budiman@gmail.com>
2019-03-12 14:01:02 +0800

    Create Node server

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const port = 8080
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log('Received request ', req)
+    console.log('route ', req.url)
+
+    if (req.url === "/watermelon") {
+        res.end('🎈')
+    } else {
+        res.end("Hello")
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+
+})
