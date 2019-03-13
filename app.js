959eb62db76c9049102d952457e666dae005f1f7
fullstackmommy <nicole.budiman@gmail.com>
2019-03-13 12:39:20 +0800

    Add built-in middleware function

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-const logger = require("./logger")
-const index = require('./routes/index')
-const students = require('./routes/students')
+//middleware
+const logger = require("./logger");
+const index = require('./routes/index');
+const students = require('./routes/students');
 
-app.use('/', index)
-app.use('/students', students)
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-app.use(logger)
+app.use(logger);
 
 app.get("/about", (req, res) => {
-    res.send("About")
-})
+    res.send("This is an about page")
+});
+
+app.post("/greet", (req, res) => {
+    const {name} = req.body
+    res.send(`Hello, ${name}`)
+});
+
+app.post("/form", (req, res) => {
+    const {name} = req.body
+    res.send(`Hello, ${name}`)
+});
+
+app.use('/students', students);
+app.use('/', index);
 
 module.exports = app;
